1a132edfcc8bc18c7e778b5d7ea37864f13526f4
gubiniliya <gubiniliya@mail.ru>
2015-11-04 02:36:58 +0300

    NEW functionsgit status!

---
+++ downloader.cpp
@@ -0,0 +1,50 @@
+//#include "вынесу часть кода в h файл" - позже
+//
+
+Downloader::Downloader(QObject *parent) : QObject(parent)
+{
+    manager = new QNetworkAccessManager(this);
+    loop = new QEventLoop();
+    connect (manager, SIGNAL(finished(QNetworkReply*)), loop, SLOT(quit()));
+    connect(manager, SIGNAL(finished(QNetworkReply*)),this, SLOT(replyFinished(QNetworkReply*)));
+    connect(this,SIGNAL(error()),this, SLOT(errorDownload()));
+}
+
+void Downloader::doDownload(QUrl url)
+{
+    QNetworkRequest request;
+    request.setUrl(QUrl(url));
+    manager->get(request);
+    loop->exec();
+}
+
+void Downloader::replyFinished (QNetworkReply *reply)
+{    
+    if(reply->error() != QNetworkReply::NoError) {
+        emit error();
+    }
+    else {
+            buff = reply->readAll();
+    }
+    reply->deleteLater();
+}
+
+Downloader::~Downloader()
+{
+    manager->deleteLater();
+}
+
+QByteArray Downloader::getBuff()
+{
+    return buff;
+}
+
+void Downloader::clearBuff()
+{
+    buff.clear();
+}
+
+void Downloader::errorDownload()
+{
+    QMessageBox::information(0, "Error", "An error while download is occured");
+}
